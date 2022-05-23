9c5d30d8f6a386c084f25444df3cfdb3cc5d3fca
Crowdbotics <team@crowdbotics.com>
2022-05-23 10:47:01 +0000

    Update components

---
+++ screens/CardSecurityModal(210:171)1280411_210_171/index.js
@@ -0,0 +1,235 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_210_172}>
+        <View style={styles.View_I210_172_451_9977} />
+      </View>
+      <View style={styles.View_210_173}>
+        <View style={styles.View_I210_173_586_264}>
+          <View style={styles.View_I210_173_586_265}>
+            <Text style={styles.Text_I210_173_586_265}>Card Security</Text>
+          </View>
+          <View style={styles.View_I210_173_586_266}>
+            <ImageBackground
+              source={{
+                uri:
+                  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/b1f1/3e8a/7d0b8fda0ccd647c16a3d5a4d7c09903"
+              }}
+              style={styles.ImageBackground_I210_173_586_266_260_1418}
+            />
+          </View>
+        </View>
+        <View style={styles.View_I210_173_586_267}>
+          <View style={styles.View_I210_173_586_269}>
+            <Text style={styles.Text_I210_173_586_269}>
+              Your card information is passed directly to Stripe, a market
+              leader in debit and credit card transactions. Circa never sees or
+              has access to your card information. To give you the highest
+              degree of privacy and security, Stripe securely stores your card
+              information and faciliates all card transactions on behalf of
+              Circa.
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.View_210_174}>
+        <View style={styles.View_I210_174_137_722} />
+      </View>
+      <ImageBackground
+        source={{
+          uri:
+            "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/6869/512b/5acd14f9d62af9750789a885c5f54059"
+        }}
+        style={styles.ImageBackground_210_175}
+      />
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(249, 250, 254, 1)" },
+  View_2: { height: hp("111%") },
+  View_210_172: {
+    width: wp("100%"),
+    height: hp("111%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I210_172_451_9977: {
+    flexGrow: 1,
+    width: wp("100%"),
+    height: hp("111%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(20, 20, 42, 1)"
+  },
+  View_210_173: {
+    width: wp("100%"),
+    height: hp("49%"),
+    minHeight: hp("49%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("61%"),
+    backgroundColor: "rgba(255, 255, 255, 1)"
+  },
+  View_I210_173_586_264: {
+    flexGrow: 1,
+    width: wp("87%"),
+    height: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("3%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I210_173_586_265: {
+    width: wp("39%"),
+    minWidth: wp("39%"),
+    minHeight: hp("4%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%"),
+    justifyContent: "flex-start"
+  },
+  Text_I210_173_586_265: {
+    color: "rgba(27, 12, 98, 1)",
+    fontSize: 19,
+    fontWeight: "900",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: -1,
+    textTransform: "none"
+  },
+  View_I210_173_586_266: {
+    width: wp("4%"),
+    minWidth: wp("4%"),
+    height: hp("2%"),
+    minHeight: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("83%"),
+    top: hp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    overflow: "hidden"
+  },
+  ImageBackground_I210_173_586_266_260_1418: {
+    flexGrow: 1,
+    width: wp("4%"),
+    height: hp("2%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    top: hp("0%")
+  },
+  View_I210_173_586_267: {
+    flexGrow: 1,
+    width: wp("87%"),
+    height: hp("30%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("6%"),
+    top: hp("11%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I210_173_586_269: {
+    width: wp("87%"),
+    top: hp("0%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    justifyContent: "flex-start"
+  },
+  Text_I210_173_586_269: {
+    color: "rgba(110, 113, 145, 1)",
+    fontSize: 13,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_210_174: {
+    width: wp("100%"),
+    height: hp("5%"),
+    top: hp("106%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("0%"),
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  View_I210_174_137_722: {
+    flexGrow: 1,
+    width: wp("36%"),
+    height: hp("1%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("32%"),
+    top: hp("3%"),
+    backgroundColor: "rgba(0, 0, 0, 1)"
+  },
+  ImageBackground_210_175: {
+    width: wp("42%"),
+    height: hp("5%"),
+    top: hp("97%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("29%")
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
